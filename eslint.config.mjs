5f71020200208ca055b84ab8e6b5c041009982e2
euclides <joaomartires2@gmail.com>
2026-09-03 18:51:41 -0300

    chore: remover prettier, editorconfig, .vscode e requirements.txt

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'eslint/config'
 import tseslint from '@electron-toolkit/eslint-config-ts'
-import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
 import eslintPluginReact from 'eslint-plugin-react'
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 import eslintPluginReactRefresh from 'eslint-plugin-react-refresh'
@@ -27,6 +26,5 @@ export default defineConfig(
       ...eslintPluginReactHooks.configs.recommended.rules,
       ...eslintPluginReactRefresh.configs.vite.rules
     }
-  },
-  eslintConfigPrettier
+  }
 )
